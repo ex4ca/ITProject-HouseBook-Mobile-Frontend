8f58cf7652a75c649dbbc771da89ed6a11c32a80
OjasManocha04 <ojas.manocha@student.unimelb.edu.au>
2025-11-03 00:01:35 +1100

    confirmation screens added locally

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,10 +1,9 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
 
-const supabaseUrl = SUPABASE_URL;
-const supabaseAnonKey = SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://qpenkaurptbxsxrysixw.supabase.co/';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwZW5rYXVycHRieHN4cnlzaXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2OTE5MTUsImV4cCI6MjA3MjI2NzkxNX0.vYnYQ_LmwwVYuaNSfu2n3KESbZi6xMLleR4-ei98Obo';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
